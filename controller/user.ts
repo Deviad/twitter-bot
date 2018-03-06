9231992d0399fc929453b0e4499e3d88b6d67dc3
Davide Pugliese <dvd.pugliese@gmail.com>
2018-03-06 22:56:08 +0200

    make controller return a valid json in case of error

--- controller/user.ts
+++ controller/user.ts
@@ -1,8 +1,6 @@
-import {
-  controller, httpGet, httpPost, httpPut, httpDelete
-} from 'inversify-express-utils';
+import * as express from 'express';
+import { interfaces, controller, httpGet, httpPost, httpPut, httpDelete, request, queryParam, response, requestParam } from 'inversify-express-utils';
 import { inject } from 'inversify';
-import { Request } from 'express';
 import { User } from '../models/user';
 import { UserService } from '../service/user';
 import TYPES from '../constant/types';
@@ -13,27 +11,47 @@ export class UserController {
   constructor( @inject(TYPES.UserService) private userService: UserService) { }
 
   @httpGet('/')
-  public getUsers(): Promise<User[]> {
-    return this.userService.find();
+  public async getUsers(@request() req: express.Request, @response() res: express.Response): Promise<User[]> {
+    try {
+      return await this.userService.find();
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
   }
 
   @httpGet('/:id')
-  public getUser(request: Request): Promise<User> {
-    return this.userService.findOneById(request.params.id);
-  }
+  public async getUser(@request() req: express.Request, @response() res: express.Response): Promise<User> {
+    try {
+      return await this.userService.findOneById(req.params.id);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+    }
 
   @httpPost('/')
-  public newUser(request: Request): Promise<User> {
-    return this.userService.insert(request.body);
-  }
+  public async newUser(@request() req: express.Request, @response() res: express.Response): Promise<User> {
+    try {
+      return await this.userService.insert(req.body);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+    }
 
   @httpPut('/:id')
-  public updateUser(request: Request): Promise<User> {
-    return this.userService.update(request.params.id, request.body);
+  public async updateUser(@request() req: express.Request, @response() res: express.Response): Promise<User> {
+    try {
+      return await this.userService.update(req.params.id, req.body);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
   }
 
   @httpDelete('/:id')
-  public deleteUser(request: Request): Promise<any> {
-    return this.userService.remove(request.params.id);
+  public async deleteUser(@request() req: express.Request, @response() res: express.Response): Promise<any> {
+    try {
+      return await this.userService.remove(req.params.id);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
   }
 }
